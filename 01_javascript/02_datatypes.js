27bb3e9385923387c98942c217efcc110b749c07
Yuvansh Vishnoi <yuvanshvishnoi@gmail.com>
2026-06-10 17:17:36 +0000

    datatypes

---
+++ 01_javascript/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all the js code as a newer version 
+//alert(8+8)
+// we are using node js not browser 
+console.log(3 + 3)
+//code readability should be high 
+console.log("yuvansh")
+let name ="yuvansh"
+let age =19
+let isloggedin=true
+/*
+numbers => 2 to power 53
+bigint
+string=>""
+boolean=> true/false
+null=>standalone value 
+undefined=>not assigned a value 
+symbol => unique
+object =>
+*/
