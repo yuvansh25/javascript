c0d3c8866fe585b51f51c7ca0209da98e715f68e
Yuvansh Vishnoi <yuvanshvishnoi@gmail.com>
2026-06-10 16:47:09 +0000

    variables

---
+++ 01_javascript/01_variables.js
@@ -0,0 +1,10 @@
+const accountid=12345
+let accountemail = "yuvi@gmail.com"
+var accountpassword = "12345"
+accountcity = "najibabad"
+let accountstate;
+//accountid = 3456
+/* prefer not to use var 
+because of issues in block and functional scope  */
+console.log(accountid);
+console.table([accountid,accountemail,accountcity,accountpassword,accountstate])
