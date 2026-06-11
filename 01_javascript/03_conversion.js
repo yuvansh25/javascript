2173b778fe4e891c89a97aa3d88ffd0939e3ef19
Yuvansh Vishnoi <yuvanshvishnoi@gmail.com>
2026-06-11 07:37:13 +0000

    datatype conversion

---
+++ 01_javascript/03_conversion.js
@@ -0,0 +1,19 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof (score));
+let string = "789";
+console.log(typeof string);
+let value =Number(string);
+console.log(typeof value);
+/*
+"33"=>number
+"33abc"=>NaN(not a number)
+"true"=>1 
+
+*/
+let isloggedin = 1;
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin);
+// true => 1 ;false =>0
+//""=> false;
+//"yuvi" => true
