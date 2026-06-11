927584a4835e100c05fb76d209271db4a7f253ae
Yuvansh Vishnoi <yuvanshvishnoi@gmail.com>
2026-06-11 08:00:05 +0000

    string and number confusion

---
+++ 01_javascript/04_operations.js
@@ -0,0 +1,19 @@
+ /* 
+ basic operations 
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2/2);
+ console.log(2**2);
+ console.log(2%2);
+ */
+let str1 = "hello"
+let str2 = " yuvansh"
+let str3 = str1+str2
+console.log(str3);
+// string to number datatype confusion
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+"2" +2);
+console.log(1+2+"2");
+console.log("1"+2+2);
