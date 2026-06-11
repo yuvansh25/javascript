1780cc32b0db5bade3d3985b78a78fc72b734df3
Yuvansh Vishnoi <yuvanshvishnoi@gmail.com>
2026-06-11 11:34:07 +0000

    comparision

---
+++ 01_javascript/05_comparision.js
@@ -0,0 +1,18 @@
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+console.log("2">1);
+ console.log(null > 0)
+ console.log(null >= 0)//here null is treated a s 0 
+ console.log(null == 0)
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);//here undefined is treated a s 0 
+
+//this is a strictier version of comparision "==="
+//it compares value as well as datatype
+
+console.log("2" > 3);
